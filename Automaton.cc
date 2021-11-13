0b3f812f77129645d22c58780a45cf698b4f0aea
Liya <liyachar@gmail.com>
2021-11-13 21:06:59 +0100

    laatste ver, bagger

--- Automaton.cc
+++ Automaton.cc
@@ -8,6 +8,7 @@ class Node{
         Node();
         Node(char c, int l, int r);
         void print();
+        Node shift(int i);
     
     private:
         char letter;
@@ -30,6 +31,15 @@ Node::Node(char c, int l, int r){
     right = r;
 }
 
+Node Node::shift(int i){
+    if (left != -1){
+        left += i;
+    }
+    if (right != -1){
+        right += i;
+    }
+}
+
 void Node::print(){
     cout << letter << ' ' << left << ' ' << right << endl;
 }
@@ -38,9 +48,9 @@ class Automaton{
     public:
         Automaton();
         Automaton(char letter);
-        Automaton concat(Automaton Aut1, Automaton Aut2);
+        void concat(Automaton Aut2);
         Automaton star();
-        Automaton add(Automaton Aut1, Automaton Aut2);
+        void add(Automaton Aut2);
         Automaton Expr();
         Automaton Term ();
         Automaton Fact ();
@@ -55,9 +65,13 @@ class Automaton{
         // char token;
         vector<Node> mat;
         int len;
+        int i;
+        int f;
 };
 
 Automaton::Automaton(){
+    i = -1;
+    f = -1;
     token = ' ';
     len = 0;
     mat.push_back(Node()); // add emtpy node at beginning
@@ -66,13 +80,23 @@ Automaton::Automaton(){
 Automaton::Automaton(char letter){
     token = ' ';
     len = 2;
+    i = 1;
+    f = 2;
     mat.push_back(Node()); // add emtpy node at beginning
-    mat.push_back(Node(letter, 1, -1));
+    mat.push_back(Node(letter, 2, -1));
     mat.push_back(Node());
 }
 
 void Automaton::next_token(){
-    cin >> token;
+    cout << "in next" << endl;
+    if (cin.eof()){
+        cout << "eof" << endl;
+        token = ' ';
+    }
+    else{
+        cin >> token;
+    }
+    cout << "NEXT: " << token << endl;
 }
 
 bool Automaton::token_is_letter(){
@@ -85,27 +109,47 @@ void Automaton::print(){
     }
 }
 
-Automaton Automaton::concat(Automaton Aut1, Automaton Aut2) {
-    // int f = Aut1.size() + Aut2.size();
-    // Aut1.mat.push_back(Node(' ', f, -1) );
-    
+void Automaton::concat(Automaton Aut2) {
+    int i2 = mat.size();
+
+
+    cout <<" what the heeel" << endl;
+    for (int i=1; i < Aut2.mat.size(); i++){
+        Node n = Aut2.mat.at(i);
+        n.shift(i2 - 1);
+        mat.push_back(n);
+    }
+    cout << "NO WAAaaAyy" << endl;
 }
 
 Automaton Automaton::star(){
-    //stuff
+
 }
 
-Automaton Automaton::add(Automaton Aut1, Automaton Aut2){
-    //stuff
+void Automaton::add(Automaton Aut2){
+    int f = mat.size() + Aut2.mat.size();
+    int i2 = mat.size();
+
+    mat.back() = Node(' ', f, -1);
+    for (int i=1; i < Aut2.mat.size(); i++){
+        Node n = Aut2.mat.at(i);
+        n.shift(i2 - 1);
+        mat.push_back(n);
+    }
+    mat.back() = Node(' ', f, -1);
+    mat.push_back(Node(' ', 1, i2) );
+    mat.push_back(Node());
 }
 
 
 Automaton Automaton::Expr() {
     Automaton Aut1 = Term();
+    Aut1.print();
     if ( token == '|' ){ 
         next_token();
         Automaton Aut2 = Expr();
-        return add(Aut1, Aut2);
+        cout << "TO ADD" << endl;
+        Aut1.add(Aut2);
     }
     return Aut1;
 } // Expr
@@ -114,11 +158,9 @@ Automaton Automaton::Term () {
     Automaton Aut1 = Fact();
     if ( token_is_letter() | (token == '(') ){
         Automaton Aut2 = Term();
-        return concat(Aut1, Aut2);
+        Aut1.concat(Aut2);
     }
     return Aut1;
-    // TODO either do not call concat if aut2 is empty, or make concat take in emtpy
-    // aut too
 } // Term
 
 Automaton Automaton::Fact () {
@@ -137,8 +179,10 @@ Automaton Automaton::Fact () {
     else if ( token_is_letter() ){
         cout << "token got" << endl;
         Aut = Automaton(token); // TODO actually make a thing
-        Aut.print();
+        // Aut.print();
+        // cout << "bb" << endl;
         next_token();
+        // cout << "aaa" << endl;
     }
     else{
         cout << "bad shit 2" << endl; // TODODOD
@@ -154,14 +198,11 @@ Automaton Automaton::Fact () {
 } // Fact
 
 int main(){
-    Automaton Aut = Automaton();
-    Aut.print();
-    Aut.next_token();
-    Aut = Aut.Fact();
-    cout << "FINIAL PRINT: "<< endl;
-    Aut.print();
-    // while (Aut.token != '$'){
-    //     cout << Aut.token << endl;
-    //     Aut.next_token();
-    // }
+    Automaton Aut1 = Automaton('a');
+    Automaton Aut2 = Automaton('b');
+    Aut1.print();
+    Aut2.print();
+    Aut1.concat(Aut2);
+    cout << "FIN" << endl;
+    Aut1.print();
 }
