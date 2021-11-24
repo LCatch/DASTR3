b95420e415828e342b35ff8f4b01a09d0c45405e
Wouter <wouter.remmerswaal@live.nl>
2021-11-24 18:54:36 +0100

    epsilon space

--- automaton.h
+++ automaton.h
@@ -10,6 +10,7 @@
 #define AUTOMATON_H
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <sstream>
@@ -40,6 +41,9 @@ class Automaton{
         void overwrite(Automaton Aut);
         void read_expr();
         void print_dot();
+
+        void e_space_rec(int n, vector<int> &e_items);
+        vector<int> e_space(vector<int> space);
 
     private:
         char token;         // stores the current token
@@ -232,4 +236,28 @@ void Automaton::print_dot(){
     out << "}";
     out.close();
 }
+
+// Add items to e_items which you can epsilon-reach via node n
+void Automaton::e_space_rec(int n, vector<int> &e_items){
+    if(find(e_items.begin(), e_items.end(), n) != e_items.end()){
+        e_items.push_back(n);
+        if(mat[n].letter==' '){
+            e_space_rec(mat[n].left, e_items);
+            if(mat[n].right!=-1){
+                e_space_rec(mat[n].right, e_items);
+            }
+        }
+    }
+} 
+
+// Return the whole epsilon space of a set of nodes 
+vector<int> Automaton::e_space(vector<int> space){
+    vector<int> new_e_space;
+    for(unsigned int i=0; i<space.size();i++){
+        e_space_rec(space[i], new_e_space);
+    }
+    return new_e_space;
+}
+
+
 #endif
